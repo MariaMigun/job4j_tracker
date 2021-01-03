2e23ae4b47c9181708a6ebd4166b0aad48f11681
Mariya <zim.masha@gmail.com>
2021-01-03 23:28:35 +0300

    all

--- src/test/java/ru/job4j/ru/job4j/oop/Cat.java
+++ src/test/java/ru/job4j/ru/job4j/oop/Cat.java
@@ -7,10 +7,35 @@ public class Cat {
         return voice;
     }
 
+    private String food;
+
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " eats " + this.food);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("Kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("black");
+        black.eat("fish");
+        black.show();
+
     }
 }
