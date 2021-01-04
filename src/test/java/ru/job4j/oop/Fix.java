7f6c4b80fc803c71382e2e755b0319cf12eaa20b
Mariya <zim.masha@gmail.com>
2021-01-04 15:46:15 +0300

    fix

--- src/test/java/ru/job4j/oop/Fix.java
+++ src/test/java/ru/job4j/oop/Fix.java
@@ -3,7 +3,12 @@ package ru.job4j.oop;
 public class Fix {
     private String desc;
 
+    public Fix(String text) {
+        desc = text;
+    }
+
     public static void main(String[] args) {
-        Fix item = new Fix();
+        String issue = "Fix me ASAP.";
+        Fix item = new Fix(issue);
     }
 }
