d6f9a4fa756266c09fe6fc78a1295d798783022d
Mariya <zim.masha@gmail.com>
2021-01-05 15:22:22 +0300

    Point

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,16 +1,19 @@
 package ru.job4j.oop;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class PointTest extends TestCase {
+public class PointTest {
 
+    @Test
     public void when111334then5() {
         Point p = new Point(1, 1, 1);
         Point d = new Point(3, 3, 4);
-        int expect = 5;
-        double dist3d = p.distance3d(d);
+        int expect = 4;
+        int dist3d = (int) p.distance3d(d);
         assertThat(dist3d, is(expect));
     }
 }
