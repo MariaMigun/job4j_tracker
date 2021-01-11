2815904c1fa175054f867f683bae2df36121916e
Mariya <zim.masha@gmail.com>
2021-01-11 23:15:29 +0300

    shop

---
+++ src/test/java/ru/job4j/oop/LiquidationProduct.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public final class LiquidationProduct extends Product {
+    private Product product;
+
+    public LiquidationProduct(String  name, int price) {
+        product = new Product(name, price);
+    }
+
+    public String label() {
+        return product.label();
+    }
+
+    /*@Override
+    public int discount() {
+        return 90;
+    }
+
+    @Override
+    public String label() {
+        return name + " " + price();
+    }*/
+}
