871f7068470b7bc836222b5ff1536153a6d151c9
Mariya <zim.masha@gmail.com>
2021-01-25 10:58:16 +0300

    shop

--- src/test/java/ru/job4j/pojo/ShopDrop.java
+++ src/test/java/ru/job4j/pojo/ShopDrop.java
@@ -3,16 +3,10 @@ package ru.job4j.pojo;
 public class ShopDrop {
 
     public static ru.job4j.oop.Product[] leftShift(ru.job4j.oop.Product[] products, int index) {
-        for (int i = 0; i < products.length; i ++){
-            if (i == index){
-                int count = products.length - 1 - index;
-                for (int j = 1; j <= count; j++){
-                    products[i] = products[i + j];
-                }
-                products[products.length - 1] = null;
+        for (int i = index; i < products.length-1; i ++){
+            products[i] = products[i + 1];
             }
-
-        }
+        products[products.length - 1] = null;
         return products;
     }
 }
