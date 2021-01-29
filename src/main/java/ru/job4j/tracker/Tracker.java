fa6e12a860d9d1361c6670a3359bb53fe7dd2be8
Mariya <zim.masha@gmail.com>
2021-01-29 12:17:14 +0300

    validation

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -41,9 +41,6 @@ public class Tracker {
             item.setId(id);
             items[index] = item;
         }
-        else {
-            System.out.println("index can not be -1");
-        }
         return rsl;
     }
     /*public boolean replace(int id, Item item) {
@@ -63,9 +60,6 @@ public class Tracker {
             System.arraycopy(items, (index + 1), items, index, (size - 1 - index));
             items[size - 1] = null;
             size--;
-        }
-        else {
-            System.out.println("index can not be -1");
         }
         return rsl;
     }
