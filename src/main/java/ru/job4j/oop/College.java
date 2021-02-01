ec688f4079151401a8494606d73c663e087430ab
Mariya <zim.masha@gmail.com>
2021-02-01 23:12:26 +0300

    student

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman fman = new Freshman();
+        Student st = fman;
+        Object obj = fman;
+    }
+}
