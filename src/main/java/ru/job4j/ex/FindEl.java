8dc57e8a9a02c51a157af33623d4534d9071ed11
Mariya <zim.masha@gmail.com>
2021-02-15 13:40:27 +0300

    exception

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,8 +4,9 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i=0; i < value.length; i++ ) {
-            if (value[i] == key){
+            if (value[i].equals(key)){
                 rsl = i;
+                break;
             }
         }
         if (rsl < 0){
@@ -22,5 +23,15 @@ public class FindEl {
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
