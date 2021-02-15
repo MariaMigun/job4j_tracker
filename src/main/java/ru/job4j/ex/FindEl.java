ec32979e3ad643089327ffaa9a0b57c29ec8d207
Mariya <zim.masha@gmail.com>
2021-02-15 12:51:18 +0300

    exception

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i=0; i < value.length; i++ ) {
+            if (value[i] == key){
+                rsl = i;
+            }
+        }
+        if (rsl < 0){
+            throw new ElementNotFoundException("There is no element");
+        }
+        return rsl;
+    }
+
+
+    public static void main(String[] args) {
+        String[] str = {"as", "asx", "ff", "ree"};
+        try {
+            indexOf(str, "ree");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
