2261d1a6413093f66ff02b0b77fa04055f72289b
Mariya <zim.masha@gmail.com>
2021-07-12 09:04:11 +0300

    job

--- src/test/java/ru/job4j/collection/JobSorterTest.java
+++ src/test/java/ru/job4j/collection/JobSorterTest.java
@@ -2,7 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 import java.util.Comparator;
-import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class JobSorterTest {
@@ -11,8 +11,8 @@ public class JobSorterTest {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 1)
         );
-        assertThat(rsl, lessThan(0));
+        assertThat(rsl, is(1));
     }
 }
