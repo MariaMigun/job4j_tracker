2750c320976fd6d638757a3565190c70abe9f6d5
Mariya <zim.masha@gmail.com>
2021-07-13 10:36:37 +0300

    user compare

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -14,12 +14,8 @@ public class User implements Comparable<User> {
     @Override
     public int compareTo(User o) {
         int rsl = this.name.compareTo(o.name);
-        if (rsl != 0) {
-           if (this.age > o.age){
-               rsl = 1;
-           } else if (this.age < o.age){
-               rsl = -1;
-           } else rsl = 0;
+        if (rsl == 0) {
+            rsl = Integer.compare(this.age, o.age);
         }
         return rsl;
     }
